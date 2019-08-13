60e94bf58e789d83901a11316bbaa9ba693b3a89
Kevin Lu <kevinlu1248@gmail.com>
2019-08-13 15:18:27 -0700

    nothings working

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,18 +1,35 @@
 $(document).ready(() => {
     $('#studentID').val(new URLSearchParams(window.location.search).get('studentID'));
-    function validateSignupForm() {
-        console.log($('.signup-form')[0].elements);
-        for (let input of $('.signup-form')[0].elements) {
-            console.log(input.checkValidity());
-            if (input.checkValidity()) {
-                $(input).addClass('is-valid');
-                $(input).removeClass('is-invalid');
-            } else {
-                $(input).addClass('is-invalid');
-                $(input).removeClass('is-valid');
-            };
-        }
-        // console.log($('.signup-form')[0].checkValidity());
-    };
-    validateSignupForm();
+    // var submitButtonPress = false;
+    // function validateInput(input) {
+    //     if (input.checkValidity()) {
+    //         $(input).addClass('is-valid');
+    //         $(input).removeClass('is-invalid');
+    //         return true;
+    //     } else {
+    //         $(input).addClass('is-invalid');
+    //         $(input).removeClass('is-valid');
+    //         return false;
+    //     };
+    // }
+    // function validateSignupForm(doUpdate = false) {
+    //     // console.log($('.signup-form')[0].elements);
+    //     if (doUpdate) {
+    //         $('.signup-form').addClass('was-validated');
+    //     }
+    //     return $('.signup-form')[0].checkValidity();
+    // };
+    // $('#signup-submit-button').click((e) => {
+    //     // console.log(validateSignupForm(true));
+    //     if (!validateSignupForm(true)) {
+    //         e.preventDefault();
+    //         submitButtonPress = true;
+    //     } else {
+    //         e.preventDefault();
+    //         $(".signup-form").submit();
+    //     }
+    // });
+    // $('.signup-form input').keyup((e) => {
+    //     validateSignupForm(submitButtonPress);
+    // });
 });
--- server.js
+++ server.js
@@ -1,27 +1,46 @@
 "use strict";
 
-const http = require('http');
+// const http = require('http');
 const mysql = require('mysql');
 const express = require('express');
+const session = require('express-session');
+var bodyParser = require('body-parser');
+const path = require('path');
 // const bootstrap = require('bootstrap');
+var favicon = require('serve-favicon');
 const app = express();
 
+// mysql conn
+var conn = mysql.createConnection({
+    host: "localhost",
+    port: "8889",
+    user: "root",
+    password: "root"
+})
+
+conn.connect((err) => {
+    if (err) throw err;
+    console.log('connected as id ' + conn.threadId);
+});
+
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
 app.use(express.static('public'));
-app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
-app.use(express.static(__dirname + '/node_modules/jquery/dist'));
-app.use(express.static(__dirname + '/node_modules/bubbly-bg/dist'));
-app.use(express.static(__dirname + '/node_modules/animate.css/'));
+app.use(express.static('views'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/js', express.static(__dirname + '/node_modules/bubbly-bg/dist'));
+app.use('/css', express.static(__dirname + '/node_modules/animate.css/'));
 app.set('view engine', 'ejs');
 
-// var con = mysql.createConnection({
-//     host: "localhost",
-//     port: "8889",
-//     user: "root",
-//     password: "root"
-// }).connect((err) => {
-//     if (err) throw err;
-//     console.log("Connected!");
-// });
+app.use(favicon(__dirname + '/views/favicon.ico'));
 
 app.get('/', (req, res) => {
     res.render('index', {path: req.originalUrl});
@@ -30,6 +49,13 @@ app.get('/', (req, res) => {
 app.get('/signup', (req, res) => {
     // res.send('Hello World');
     res.render('signup', {path: req.originalUrl});
+})
+
+app.post('/auth', (req, res) => {
+    // res.send('Hello World');
+    console.log(req.body);
+    response.send('Please enter Username and Password!');
+    response.end();
 })
 
 // PORT
